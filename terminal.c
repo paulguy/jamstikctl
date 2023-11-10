14c8395f1d3b6ecdc41fef9bf1abaeaeee39a455
Paul LaMendola <paulguy119@gmail.com>
2023-11-10 17:25:46 -0500

    Rewrite terminal functions to use ncurses.

--- terminal.c
+++ terminal.c
@@ -18,9 +18,12 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
 #include <curses.h>
+#include <wchar.h>
+/*
 #include <termios.h>
 #include <term.h>
 #include <errno.h>
@@ -28,172 +31,19 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <signal.h>
-#include <stdlib.h>
-#include <wchar.h>
-
-/* TODO: redo everything */
+*/
 
 typedef struct {
-    int only_print;
+    WINDOW *main_term;
+    WINDOW *notes_term;
+    WINDOW *status_term;
 
-    int stdout_fd;
-    int stdin_fd;
-    struct termios original_termios;
-    struct sigaction ohup;
-    struct sigaction oint;
-    struct sigaction oterm;
-
-    int termlines, termcolumns;
-
-    char *home;
-    char *clear;
-    char *dl1;
-    char *il1;
-
-    char *laststr;
-    int lastlen;
     int lastlines;
 } terminal_ctx_t;
 
 terminal_ctx_t termctx;
 
-void term_home() {
-    fputs(termctx.home, stdout);
-}
-
-void term_bottom() {
-    int i;
-
-    term_home();
-    for(i = 0; i < termctx.termlines; i++) {
-        fputc('\n', stdout);
-    }
-}
-
-void term_clear() {
-    fputs(termctx.clear, stdout);
-}
-
-void term_clear_line() {
-    fputs(termctx.dl1, stdout);
-    fputs(termctx.il1, stdout);
-}
-
-void term_cleanup() {
-    int ret;
-    unsigned int termlines;
-    unsigned int i;
-
-    /* push status messages past where the terminal may clear it away */
-    if(!termctx.only_print) {
-        termlines = tigetnum("lines");
-        for(i = 0; i < termlines * 2; i++) {
-            fputc('\n', stdout);
-        }
-    }
-
-    ret = tcsetattr(termctx.stdout_fd, TCSADRAIN, &termctx.original_termios);
-    if(ret < 0) {
-        fprintf(stderr, "Couldn't reset termios: %s\n", strerror(errno));
-    }
-
-    if(sigaction(SIGHUP, &termctx.ohup, NULL) != 0 ||
-       sigaction(SIGINT, &termctx.oint, NULL) != 0 ||
-       sigaction(SIGTERM, &termctx.oterm, NULL) != 0) {
-        fprintf(stderr, "Failed to set signal handler.\n");
-    }
-}
-
-static void term_cleanup_handler(int signum) {
-    term_cleanup();
-    if(signum == SIGHUP && termctx.ohup.sa_handler != NULL) {
-        termctx.ohup.sa_handler(signum);
-    } else if(signum == SIGINT && termctx.oint.sa_handler != NULL) {
-        termctx.oint.sa_handler(signum);
-    } else if(signum == SIGTERM && termctx.oterm.sa_handler != NULL) {
-        termctx.oterm.sa_handler(signum);
-    }
-}
-
-int term_setup(int only_print) {
-    struct sigaction sa;
-    sa.sa_handler = term_cleanup_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-
-    struct termios new_termios;
-
-    termctx.only_print = only_print;
-    termctx.stdout_fd = fileno(stdout);
-    termctx.stdin_fd = fileno(stdin);
-    termctx.laststr = NULL;
-    termctx.lastlines = 0;
-
-    if(tcgetattr(termctx.stdout_fd, &termctx.original_termios) < 0) {
-        fprintf(stderr, "Couldn't get termios: %s\n", strerror(errno));
-        return(-1);
-    }
-
-    if(sigaction(SIGHUP, &sa, &termctx.ohup) != 0 ||
-       sigaction(SIGINT, &sa, &termctx.oint) != 0 ||
-       sigaction(SIGTERM, &sa, &termctx.oterm) != 0) {
-        fprintf(stderr, "Failed to set signal handler.\n");
-    }
-
-    memcpy(&new_termios, &termctx.original_termios, sizeof(struct termios));
-    cfmakeraw(&new_termios);
-    /* allow stuff like CTRL+C to work */
-    new_termios.c_lflag |= ISIG;
-    /* allow printed text to not look weird */
-    new_termios.c_oflag |= OPOST;
-    new_termios.c_cc[VMIN] = 1;
-    new_termios.c_cc[VTIME] = 0;
-
-    if(tcsetattr(termctx.stdout_fd, TCSADRAIN, &new_termios) < 0) {
-        fprintf(stderr, "Couldn't set termios: %s\n", strerror(errno));
-        return(-1);
-    }
-
-    if(!termctx.only_print) {
-        if(setupterm(NULL, termctx.stdout_fd, NULL) == ERR) {
-            return(-1);
-        }
-
-        termctx.home = tigetstr("home");
-        termctx.clear = tigetstr("clear");
-        termctx.dl1 = tigetstr("dl1");
-        termctx.il1 = tigetstr("il1");
-        termctx.termlines = tigetnum("lines");
-        termctx.termcolumns = tigetnum("columns");
-
-        term_clear();
-    }
-
-    return(0);
-}
-
-int term_print_mode() {
-    return(termctx.only_print);
-}
-
-int term_getkey() {
-    fd_set readset;
-    struct timeval tv;
-
-    FD_ZERO(&readset);
-    FD_SET(termctx.stdin_fd, &readset);
-    tv.tv_sec = 0;
-    tv.tv_usec = 0;
-
-    select(termctx.stdin_fd+1, &readset, 0, 0, &tv);
-    if(FD_ISSET(termctx.stdin_fd, &readset)) {
-        return(fgetc(stdin));
-    }
-
-    return(-1);
-}
-
-int term_count_lines(int n, const char *str) {
+int term_count_lines(int textwidth, int n, const char *str) {
     int strlines = 0;
     int strcolumns = 0;
     int i;
@@ -255,13 +105,30 @@ int term_count_lines(int n, const char *str) {
             c = str[i];
         }
 
+        if(c == '\0') {
+            break;
+        }
+
+        if(strlines == 0) {
+            /* an empty string should be 0 lines, but at least 1 decoded
+             * character implies at least 1 line. */
+            strlines = 1;
+        }
+
+        if(c == '\n') {
+            strlines++;
+            strcolumns = 0;
+            continue;
+        }
+
         width = wcwidth(c);
 
         strcolumns += width;
-        if(strcolumns == termctx.termcolumns) {
+        /* don't put a new line if the text string just fits the width */
+        if(strcolumns == textwidth && i < n) {
             strlines++;
             strcolumns = 0;
-        } else if(strcolumns > termctx.termcolumns) {
+        } else if(strcolumns > textwidth) {
             strlines++;
             strcolumns = width;
         }
@@ -270,85 +137,132 @@ int term_count_lines(int n, const char *str) {
     return(strlines);
 }
 
-int term_clear_status_area() {
-    int termlines;
-    int temp;
-    int i;
+char *term_get_string_and_lines(int *lines, int *len,
+                                const char *f, va_list ap) {
+    char *str;
+    va_list ap2;
+    int n;
 
-    termlines = term_count_lines(termctx.lastlen, termctx.laststr);
-    if(termlines < 0) {
+    va_copy(ap2, ap);
+    n = vsnprintf(NULL, 0, f, ap2);
+    va_end(ap2);
+
+    str = malloc(n + 1);
+    if(str == NULL) {
+        return(NULL);
+    }
+
+    va_copy(ap2, ap);
+    *len = vsnprintf(str, n+1, f, ap2);
+    va_end(ap2);
+    if(*len != n) {
+        free(str);
+        return(NULL);
+    }
+
+    *lines = term_count_lines(COLS, n, str);
+    if(*lines <= 0) {
+        free(str);
+        return(NULL);
+    }
+    
+    return(str);
+}
+
+void term_cleanup() {
+    if(termctx.main_term != NULL) {
+        termctx.main_term = NULL;
+        delwin(termctx.status_term);
+        termctx.status_term = NULL;
+        delwin(termctx.notes_term);
+        termctx.notes_term = NULL;
+        endwin();
+    }
+}
+
+int term_setup(int only_print) {
+    termctx.main_term = NULL;
+
+    if(only_print) {
+        termctx.main_term = initscr();
+        if(termctx.main_term == NULL) {
+            goto error;
+        }
+        /* some recommended otpions I guess */
+        cbreak();
+        noecho();
+        intrflush(termctx.main_term, FALSE);
+        /* don't want delays */
+        nodelay(termctx.main_term, TRUE);
+
+        termctx.status_term = newwin(LINES, COLS, 1, 0);
+        if(termctx.status_term == NULL) {
+            goto error_main_term;
+        }
+        idlok(termctx.status_term, TRUE);
+        scrollok(termctx.status_term, TRUE);
+        termctx.notes_term = newwin(1, COLS, 0, 0);
+        if(termctx.notes_term == NULL) {
+            goto error_status_term;
+        }
+        termctx.lastlines = 1;
+    }
+
+    return(0);
+
+error_status_term:
+    delwin(termctx.status_term);
+    termctx.status_term = NULL;
+error_main_term:
+    endwin();
+    termctx.main_term = NULL;
+error:
+    return(-1);
+}
+
+int term_print_mode() {
+    return(termctx.main_term == NULL);
+}
+
+int term_getkey() {
+    int key;
+
+    key = getch();
+    
+    if(key == ERR) {
         return(-1);
     }
 
-    if(termctx.lastlines > termlines) {
-        /* get the larger of the two but still store the new value */
-        temp = termctx.lastlines;
-        termctx.lastlines = termlines;
-        termlines = temp;
-    } else {
-        termctx.lastlines = termlines;
-    }
-
-    term_home();
-    for(i = 0; i < termlines; i++) {
-        term_clear_line();
-        fputc('\n', stdout);
-    }
-
-    return(0);
-}
-
-int term_check_size() {
-    int termlines, termcolumns;
-
-    if(!termctx.only_print) {
-        termlines = tigetnum("lines");
-        termcolumns = tigetnum("columns");
-
-        if(termlines != termctx.termlines ||
-           termcolumns != termctx.termcolumns) {
-            termctx.termlines = termlines;
-            termctx.termcolumns = termcolumns;
-
-            term_clear();
-            term_home();
-
-            return(1);
-        }
-    }
-
-    return(0);
+    return(key);
 }
 
 int term_print(const char *f, ...) {
     int n;
+    int strlines;
     va_list ap;
-    int cleared;
+    char *str;
 
-    if(!termctx.only_print) {
-        cleared = term_check_size();
-
-        term_bottom();
-
-        va_start(ap, f);
-        n = vfprintf(stdout, f, ap);
-        va_end(ap);
-
-        if(!cleared) {
-            if(term_clear_status_area() < 0) {
-                termctx.only_print = 1;
-                return(n);
-            }
-        }
-        term_home();
-        if(termctx.laststr != NULL) {
-            fputs(termctx.laststr, stdout);
-        }
-    } else {
+    if(term_print_mode()) {
         va_start(ap, f);
         n = vfprintf(stdout, f, ap);
         va_end(ap);
         fputc('\n', stdout);
+    } else {
+        /* push messages from the bottom */
+        va_start(ap, f);
+        str = term_get_string_and_lines(&strlines, &n, f, ap);
+        va_end(ap);
+
+        if(str == NULL) {
+            return(-1);
+        }
+
+        wscrl(termctx.status_term, strlines);
+        mvwaddnstr(termctx.status_term, LINES-termctx.lastlines-strlines, 0,
+                   str, n);
+        free(str);
+
+        wrefresh(termctx.status_term);
     }
 
     return(n);
@@ -358,55 +272,43 @@ int term_print_static(const char *f, ...) {
     int n;
     va_list ap;
     char *str;
+    int strlines;
 
-    if(!termctx.only_print) {
-        if(termctx.laststr != NULL) {
-            /*
-            if(term_clear_status_area() < 0) {
-                termctx.only_print = 1;
-
-                va_start(ap, f);
-                n = vfprintf(stdout, f, ap);
-                va_end(ap);
-
-                return(n);
-            }
-            */
-
-            free(termctx.laststr);
-            termctx.laststr = NULL;
-        }
-
-        va_start(ap, f);
-        termctx.lastlen = vsnprintf(NULL, 0, f, ap);
-        va_end(ap);
-
-        str = malloc(termctx.lastlen + 1);
-        if(str == NULL) {
-            return(-1);
-        }
-
-        va_start(ap, f);
-        n = vsnprintf(str, termctx.lastlen, f, ap);
-        va_end(ap);
-        if(n != termctx.lastlen) {
-            goto error;
-        }
-
-        term_home();
-        fputs(str, stdout);
-
-        termctx.laststr = str;
-    } else {
+    if(term_print_mode()) {
         va_start(ap, f);
         n = vfprintf(stdout, f, ap);
         va_end(ap);
         fputc('\n', stdout);
+    } else {
+        va_start(ap, f);
+        str = term_get_string_and_lines(&strlines, &n, f, ap);
+        va_end(ap);
+
+        if(str == NULL) {
+            return(-1);
+        }
+
+        if(strlines != termctx.lastlines) {
+            if(strlines > termctx.lastlines) {
+                wscrl(termctx.status_term, strlines - termctx.lastlines);
+            }
+            wresize(termctx.notes_term, strlines, COLS);
+            wresize(termctx.status_term, LINES - strlines, COLS);
+            mvwin(termctx.status_term, strlines, 0);
+            if(strlines < termctx.lastlines) {
+                wscrl(termctx.status_term, termctx.lastlines - strlines);
+            }
+            wrefresh(termctx.status_term);
+
+            termctx.lastlines = strlines;
+        }
+        wclear(termctx.notes_term);
+
+        mvwaddnstr(termctx.notes_term, 0, 0, str, n);
+        free(str);
+
+        wrefresh(termctx.notes_term);
     }
 
     return(n);
-
-error:
-    free(str);
-    return(-1);
 }
